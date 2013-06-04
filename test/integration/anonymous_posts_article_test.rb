6d848f1e32cbad283666c5622bc0729561354ca0
dmoore5050 <dustinmoore@gmail.com>
2013-06-04 10:33:04 -0500

    User browses list of posts

--- test/integration/anonymous_posts_article_test.rb
+++ test/integration/anonymous_posts_article_test.rb
@@ -6,15 +6,11 @@ class AnonymousPostsArticleTest < ActionDispatch::IntegrationTest
     Post.create!(title: "First Post!!", body: "This is awkward... I have nothing to say :(")
     Post.create!(title: "Wow, it's been awhile!", body: "And yet, I'm still not a writer :( :( lolol")
 
-    visit '/'
-    click link 'View all posts'
-    assert_current_path '/posts'
-    #assume we have a page variable
-    assert_include page.content, 'First Post!!'
-    assert_include page.content, 'Wow, it\'s been awhile!'
+    visit '/posts'
+
+    assert_include page.body, 'All Posts'
+    assert_include page.body, 'First Post!!'
+    assert_include page.body, 'Wow, it\'s been awhile!'
   end
 
-  # test "the truth" do
-  #   assert true
-  # end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,9 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 
+require 'capybara/rails'
+Capybara.default_driver = :selenium
+
 class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
 end
@@ -9,6 +12,5 @@ end
 class ActionDispatch::IntegrationTest
   # Make the Capybara DSL available in all integration tests
   include Capybara::DSL
+  self.use_transactional_fixtures = false
 end
-
-Capybara.default_driver = :selenium
